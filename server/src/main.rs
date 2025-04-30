0df3bad7592e02cd89fa5aedd2cf0a7b9410dfe3
tutla53 <ayatullah_tutla@yahoo.com>
2025-04-30 11:34:04 +0700

    Add up time counter to see if the pico firmware is hanging

--- server/src/main.rs
+++ server/src/main.rs
@@ -11,9 +11,9 @@ use {
         BmeResources,
     },
     embassy_executor::Spawner,
-    embassy_time::{Duration, Timer},
+    embassy_time::{Duration, Timer, Instant, Ticker},
     embassy_sync::{
-        mutex::Mutex,
+        channel::Channel,
         blocking_mutex::raw::CriticalSectionRawMutex,
     },
     embassy_rp::{
@@ -42,24 +42,14 @@ struct BmeData {
 }
 
 struct BmeState {
-    state : Mutex<CriticalSectionRawMutex, BmeData>,
+    state : Channel<CriticalSectionRawMutex, BmeData, 100>,
 }
 
 impl BmeState {
     const fn new() -> Self {
         Self {
-            state: Mutex::new(
-                BmeData{
-                    temperature: 0.0, 
-                    humidity: 0.0, 
-                    pressure: 0.0
-                }
-            ),
+            state: Channel::new(),
         }
-    }
-
-    async fn get_data(&self) -> BmeData {
-        return *self.state.lock().await;
     }
 
     async fn set_data(&self, temperature:f32, humidity:f32, pressure:f32) {
@@ -68,13 +58,52 @@ impl BmeState {
             humidity:humidity, 
             pressure:pressure
         };
-        let mut current_state = self.state.lock().await;
-        *current_state = new_state;
+
+        let _ = self.state.try_send(new_state);
+    }
+}
+
+#[derive(Clone, Copy)]
+struct TimeFormat {
+    days: u64,
+    hours: u64,
+    minutes: u64,
+    seconds: u64,
+}
+
+struct UpTime {
+    state : Channel<CriticalSectionRawMutex, TimeFormat, 100>,
+}
+
+impl UpTime {
+    const fn new() -> Self {
+        Self {
+            state: Channel::new(),
+        }
     }
 
+    async fn set_data(&self, duration: embassy_time::Duration) {
+        let total_seconds = duration.as_secs();
+        let days = total_seconds / 86400;  
+        let remaining = total_seconds % 86400;
+        let hours = remaining / 3600;
+        let remaining = remaining % 3600;
+        let minutes = remaining / 60;
+        let seconds = remaining % 60;
+
+        let new_state = TimeFormat {
+            days: days,
+            hours: hours, 
+            minutes: minutes, 
+            seconds: seconds
+        };
+
+        let _ = self.state.try_send(new_state);
+    }
 }
 
 static BME: BmeState = BmeState::new();
+static UPTIME: UpTime = UpTime::new();
 
 #[embassy_executor::task]
 async fn usb_logger_task(driver: Driver<'static, USB>) {
@@ -104,29 +133,35 @@ async fn display_task(p: DisplayResources) {
     display.clear().await.unwrap();
     display.set_position(2, 0).await.unwrap();
     let _ = display.write_str("Air Monitor").await;
+    let mut tick = Ticker::every(Duration::from_millis(500));
 
     loop {
-        let data = BME.get_data().await;
+        if let Ok(uptime) = UPTIME.state.try_receive() {  
+            let mut uptime_str = String::<32>::new();
+            write!(&mut uptime_str,   "{}d:{:02}h:{:02}m:{:02}s", uptime.days, uptime.hours, uptime.minutes, uptime.seconds).unwrap(); 
+            display.set_position(0, 4).await.unwrap();
+            let _ = display.write_str(&uptime_str).await;
+        }
 
-        let mut ip_str = String::<32>::new();
-        let mut temp_str = String::<32>::new();
-        let mut humidity_str = String::<32>::new();
-        let mut pressure_str = String::<32>::new();
-        
-        write!(&mut ip_str, "No WIFI").unwrap();
-        write!(&mut temp_str, "Temp: {:.2} C", data.temperature).unwrap();
-        write!(&mut humidity_str, "Humidity: {:.1}%", data.humidity).unwrap();
-        write!(&mut pressure_str, "P: {:.3} atm", data.pressure).unwrap();
+        if let Ok(data) = BME.state.try_receive() {
+            let mut temp_str = String::<32>::new();
+            let mut humidity_str = String::<32>::new();
+            let mut pressure_str = String::<32>::new();
+            
+            
+            write!(&mut temp_str,     "Temp:{:.2} C", data.temperature).unwrap();
+            write!(&mut humidity_str, "RH  :{:.2} %", data.humidity).unwrap();
+            write!(&mut pressure_str, "P   :{:.3} atm", data.pressure).unwrap();
 
-        display.set_position(0, 2).await.unwrap();
-        let _ = display.write_str(&ip_str).await;
-        display.set_position(0, 4).await.unwrap();
-        let _ = display.write_str(&temp_str).await;
-        display.set_position(0, 5).await.unwrap();
-        let _ = display.write_str(&humidity_str).await;
-        display.set_position(0, 6).await.unwrap();
-        let _ = display.write_str(&pressure_str).await;
-        Timer::after(Duration::from_secs(1)).await;
+            display.set_position(0, 5).await.unwrap();
+            let _ = display.write_str(&temp_str).await;
+            display.set_position(0, 6).await.unwrap();
+            let _ = display.write_str(&humidity_str).await;
+            display.set_position(0, 7).await.unwrap();
+            let _ = display.write_str(&pressure_str).await;
+        }
+
+        tick.next().await;
     }
 }
 
@@ -150,6 +185,8 @@ async fn bme_task(p: BmeResources) {
         Timer::after(Duration::from_millis(500)).await;
     }
 
+    let mut tick = Ticker::every(Duration::from_millis(500));
+
     loop {
         match bme280.measure(&mut delay) {
             Ok(data) => {
@@ -172,7 +209,8 @@ async fn bme_task(p: BmeResources) {
                 }
             }
         }
-        Timer::after(Duration::from_millis(500)).await;
+
+        tick.next().await;
     }
 }
 
@@ -185,5 +223,12 @@ async fn main(spawner: Spawner) {
     let ph = split_resources!(p);
     spawner.must_spawn(display_task(ph.display_resources));
     spawner.must_spawn(bme_task(ph.bme_resources));
-
+    
+    let mut tick = Ticker::every(Duration::from_secs(1));
+    let uptime = Instant::now();
+    
+    loop {
+        UPTIME.set_data(uptime.elapsed()).await;
+        tick.next().await;
+    }
 }
